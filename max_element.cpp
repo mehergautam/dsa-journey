285aa4dcc56c1d5e1caec556f2354a6fbcc55e9f
Meher Mishra <mehergautam.dev@gmail.com>
2026-02-12 21:51:14 +0530

    Create max_element.cpp

---
+++ max_element.cpp
@@ -0,0 +1,29 @@
+// Problem: Find maximum element in an array
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int maxElement = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    cout << maxElement;
+
+    return 0;
+}
