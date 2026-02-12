bcc666416333ef8db04d556c7ea6639703a735f4
Meher Mishra <mehergautam.dev@gmail.com>
2026-02-12 21:58:50 +0530

    Day 2: Added min element program

---
+++ array/min_element.cpp
@@ -0,0 +1,29 @@
+// Problem: Find minimum element in an array
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int minElement = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] < minElement) {
+            minElement = arr[i];
+        }
+    }
+
+    cout << minElement;
+
+    return 0;
+}
