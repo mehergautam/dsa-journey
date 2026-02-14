609c2a9b8ed82f5eef099bd30ca095fa6614107e
Meher Mishra <mehergautam.dev@gmail.com>
2026-02-14 13:38:37 +0530

    Day 4: Implemented Insertion Sort

---
+++ sorting/insertion_sort.cpp
@@ -0,0 +1,39 @@
+// Problem: Insertion Sort
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Insertion Sort
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
