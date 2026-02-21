766a0117da2a1f2f92f50300334fac72a4fd9abc
Meher Mishra <mehergautam.dev@gmail.com>
2026-02-21 23:04:35 +0530

    Implemented basic Binary Tree structure

---
+++ tree/binary_tree_basic.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = right = NULL;
+    }
+};
+
+int main() {
+
+    // Creating a simple binary tree manually
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "Binary Tree Created Successfully" << endl;
+
+    return 0;
+}
