68a3677a8e6db1bf7116f4e967070c66e03e2ef9
Meher Mishra <mehergautam.dev@gmail.com>
2026-02-22 23:04:07 +0530

    DAY 11: Structured Tree Module and Implemented Diameter (Optimized)

---
+++ tree/diameter.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int solve(Node* root, int &diameter) {
+    if (root == NULL) return 0;
+
+    int leftHeight = solve(root->left, diameter);
+    int rightHeight = solve(root->right, diameter);
+
+    diameter = max(diameter, leftHeight + rightHeight);
+
+    return 1 + max(leftHeight, rightHeight);
+}
+
+int diameterOfBinaryTree(Node* root) {
+    int diameter = 0;
+    solve(root, diameter);
+    return diameter;
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "Diameter: " << diameterOfBinaryTree(root);
+
+    return 0;
+}
