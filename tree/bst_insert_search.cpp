762b4c41d1f93bfa9100d09bd596f9f3a244d937
Meher Mishra <mehergautam.dev@gmail.com>
2026-02-24 16:04:40 +0530

    DAY 14: Started Binary Search Tree (Insert and Search)

---
+++ tree/bst_insert_search.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root, int key) {
+    if (root == NULL)
+        return new Node(key);
+
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else
+        root->right = insert(root->right, key);
+
+    return root;
+}
+
+bool search(Node* root, int key) {
+    if (root == NULL) return false;
+
+    if (root->data == key)
+        return true;
+
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+int main() {
+    Node* root = NULL;
+
+    root = insert(root, 5);
+    insert(root, 3);
+    insert(root, 7);
+    insert(root, 2);
+    insert(root, 4);
+
+    if (search(root, 4))
+        cout << "Found";
+    else
+        cout << "Not Found";
+
+    return 0;
+}
