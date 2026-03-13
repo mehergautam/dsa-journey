9f6196b5021d5f3e498f8c16d45afc6161e7bd10
Meher Mishra <mehergautam.dev@gmail.com>
2026-03-13 23:27:10 +0530

    DAY 32: Implemented Top K Frequent Elements using Min Heap

---
+++ heap/top_k_frequent_elements.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+vector<int> topKFrequent(vector<int>& nums, int k) {
+
+    unordered_map<int,int> freq;
+
+    for(int x:nums)
+        freq[x]++;
+
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+    for(auto it:freq){
+
+        pq.push({it.second,it.first});
+
+        if(pq.size()>k)
+            pq.pop();
+    }
+
+    vector<int> result;
+
+    while(!pq.empty()){
+        result.push_back(pq.top().second);
+        pq.pop();
+    }
+
+    return result;
+}
+
+int main(){
+
+    vector<int> nums = {1,1,1,2,2,3};
+    int k = 2;
+
+    vector<int> ans = topKFrequent(nums,k);
+
+    for(int x:ans)
+        cout<<x<<" ";
+
+}
