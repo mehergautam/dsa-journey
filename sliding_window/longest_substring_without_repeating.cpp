813fc1370d799eb7ea9ce41e597123c1753fbd57
Meher Mishra <mehergautam.dev@gmail.com>
2026-03-18 23:01:08 +0530

    DAY 35: Solved Longest Substring Without Repeating Characters using Sliding Window
    Implement function to find the length of the longest substring without repeating characters.

---
+++ sliding_window/longest_substring_without_repeating.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int longestSubstring(string s){
+
+    unordered_set<char> st;
+    int left = 0, maxLen = 0;
+
+    for(int right = 0; right < s.size(); right++){
+
+        while(st.find(s[right]) != st.end()){
+            st.erase(s[left]);
+            left++;
+        }
+
+        st.insert(s[right]);
+        maxLen = max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+}
+
+int main(){
+
+    string s = "abcabcbb";
+
+    cout<<"Length: "<<longestSubstring(s);
+}
